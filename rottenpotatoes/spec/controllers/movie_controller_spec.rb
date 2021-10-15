132e3f69f2daa8296b6baa38dd9a5f607251b204
evelyn.crowe <evelyn.a.crowe@gmail.com>
2021-10-15 17:39:52 +0000

    movie_spec functinoality

--- rottenpotatoes/spec/controllers/movie_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movie_controller_spec.rb
@@ -8,5 +8,6 @@ describe MoviesController do
             Movie.create(:title => 'Alien', :rating => 'R', :director => "")
             Movie.create(:title => 'ThX-1138', :rating => 'R', :director => "George Lucas")
         end
+        
     end
 end
--- rottenpotatoes/spec/models/movie_spec.rb
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -8,5 +8,17 @@ describe Movie do
             Movie.create(:title => 'Alien', :rating => 'R', :director => "")
             Movie.create(:title => 'ThX-1138', :rating => 'R', :director => "George Lucas")
         end
+        it 'should find movies with same director' do
+            expect(Movie.where(director: "Ridley Scott").length).to eq(1)
+            expect(Movie.where(director: "George Lucas").length).to eq(2)
+            expect(Movie.where(director: "").length).to eq(1)
+        end
+        it 'should not find movies with different directors' do
+            Movie.where(director: "Ridley Scott").each { |movie| 
+                expect(movie.director).to eq("Ridley Scott")
+                expect(movie.director).to_not eq("George Lucas")
+                expect(movie.director).to_not eq("")
+            }
+        end
     end
 end
